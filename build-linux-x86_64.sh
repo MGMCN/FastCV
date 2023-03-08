abcc570cadd039f14aec8c9e19964e4bc05628d3
Js <yohogg2858@gmail.com>
2023-03-09 01:11:11 +1100

    revise shell

--- build-linux-x86_64.sh
+++ build-linux-x86_64.sh
@@ -1,7 +1,7 @@
 sudo apt update;
 sudo apt upgrade;
 sudo apt install git;
-sudo apt install hugo=0.92.2;
+sudo apt install hugo=0.68.3-1;
 mkdir hugoServer;
 hugo new site hugoServer/mycv;
 git clone https://gitlab.com/mertbakir/resume-a4.git hugoServer/mycv/themes/resume-a4;
